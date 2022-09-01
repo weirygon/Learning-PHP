4a5f338728c3387a5b3ad78327ed22536aff4c0a
Weiry Gonçalves <33106370+weirygon@users.noreply.github.com>
2022-08-31 23:34:38 -0300

    Add files via upload

---
+++ L1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List 1</title>
+</head>
+<body>
+
+    <div>
+        
+        <?php
+	
+        //EX 01
+    
+        $a = 10;
+        $b = '10';
+    
+        echo $a + $b;
+    
+        echo "<hr>"; //Break line
+    
+        
+        ?>
+        
+    </div>
+
+    <div>
+        
+    </div>
+    
+</body>
+</html>
